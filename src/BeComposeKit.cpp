e642440d937aeab6dc6526690e48893cafcdd553
Andy <andy@quietcode.org>
2026-05-31 10:46:27 +0200

    Add native library skeleton

---
+++ src/BeComposeKit.cpp
@@ -0,0 +1,11 @@
+#include <BeComposeKit/BeComposeKit.h>
+
+namespace BeComposeKit {
+
+const char*
+VersionString()
+{
+	return "0.1.0";
+}
+
+} // namespace BeComposeKit
---
+++ tests/version_smoke.cpp
@@ -0,0 +1,10 @@
+#include <BeComposeKit/BeComposeKit.h>
+
+#include <stdio.h>
+
+int
+main()
+{
+	printf("BeComposeKit %s\n", BeComposeKit::VersionString());
+	return 0;
+}
